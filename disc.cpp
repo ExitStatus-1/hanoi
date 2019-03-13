e81c444a4f5c3947727be35e85d1446ebef6e651
ExitStatus-1 <46453838+ExitStatus-1@users.noreply.github.com>
2019-03-13 12:11:13 -0500

    Create disc.cpp

---
+++ disc.cpp
@@ -0,0 +1,31 @@
+#include "disc.h"
+
+Disc::Disc()
+{
+    size = 0;
+}
+
+Disc::Disc(int s)
+{
+    size = s;
+}
+
+Disc::~Disc()
+{
+}
+
+int Disc::getSize()
+{
+    return size;
+}
+
+bool Disc::setSize(int s)
+{
+    size = s;
+    return true;
+}
+
+std::string Disc::toString()
+{
+    return std::to_string(size);
+}
